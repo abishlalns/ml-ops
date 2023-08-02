55ec96e2fd14ebec36db76b3e701c8dedb22c2d3
Abishlal Nelson <abishlal.nelson@solitontech.local>
2023-08-02 18:57:17 +0530

    testing
    Signed-off-by: Abishlal Nelson <abishlal.nelson@solitontech.local>

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from transformers import pipeline, set_seed
 generator = pipeline('text-generation', model='gpt2')
 set_seed(42)
-print(generator("Hi GPT", max_length=10))
+print(generator("Hi GPT, ", max_length=10))
